18e3dc205182aa2d617778a85db5d35767d5584d
seagullsay <seagullsay3@yahoo.com>
2016-06-04 15:02:45 +0800

    update

--- app/crawler/run_spider.py
+++ app/crawler/run_spider.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from threading import Timer
+import time
 from .wccftech_spider import run_wccftech
 from .gnews_crawler import run_gnews
 from .dualshockers import run_dualshockers
@@ -7,11 +7,15 @@ from multiprocessing import Process
 
 def run():
     p1 = Process(target=run_gnews)
-p2 = Process(target=run_wccftech)
-p4 = Process(target=run_dualshockers)
-p1.start()
-p2.start()
-p4.start()
-p1.join()
-p2.join()
-p4.join()
+    p2 = Process(target=run_wccftech)
+    p4 = Process(target=run_dualshockers)
+    p1.start()
+    p2.start()
+    p4.start()
+    p1.join()
+    p2.join()
+    p4.join()
+
+while True:
+    run()
+    time.sleep(10800)
